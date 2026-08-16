b0acd84f44044be70a358b53098f36fd55557594
joo420 <jeromjoshep.23@gmail.com>
2026-08-17 02:02:10 +0530

    Fix 16-08-2026

--- nugens-platform/apps/portfolio/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import ProofOfWork from "./pages/ProofOfWork";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ProofOfWork />
-  </React.StrictMode>
-);
